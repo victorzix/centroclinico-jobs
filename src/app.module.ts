b45fab905f31a581a1b694baa5cf74b1d6eb312b
victorzix <linkgamesplays@gmail.com>
2025-05-05 09:16:38 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
